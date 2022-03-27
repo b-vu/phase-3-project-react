6b410af4546acc8463db679bea3d44fb58a0bb8c
Brian Vu <brianvu7@gmail.com>
2022-03-26 19:24:40 -0500

    added base code

--- project-3-frontend/src/App.js
+++ project-3-frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react"
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const { user, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    fetch("http://localhost:9292/")
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? data.message : null}
+      {isAuthenticated ?
+        <>
+          <LogoutButton />
+          <Profile user={user}/>
+        </>
+        : <LoginButton />}
     </div>
   );
 }
 
 export default App;
+
